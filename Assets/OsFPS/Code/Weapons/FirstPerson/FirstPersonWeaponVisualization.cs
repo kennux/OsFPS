04f64212a1f3c9f59aefd39e4a40cef611858c85
Kenneth <kennux@ymail.com>
2018-08-09 22:25:04 +0200

    Implemented empty magazin animation state for pistol HK45

--- Assets/OsFPS/Code/Weapons/FirstPerson/FirstPersonWeaponVisualization.cs
+++ Assets/OsFPS/Code/Weapons/FirstPerson/FirstPersonWeaponVisualization.cs
@@ -11,13 +11,42 @@ namespace OsFPS
     /// </summary>
     public class FirstPersonWeaponVisualization : WeaponVisualization
     {
+        /// <summary>
+        /// The nimation component to use.
+        /// </summary>
         [Header("Animation")]
         public new Animation animation;
+
+        /// <summary>
+        /// The reload animation name.
+        /// </summary>
         public string reloadAnimation;
+
+        /// <summary>
+        /// Firing animation name.
+        /// </summary>
         public string fireAnimation;
 
+        /// <summary>
+        /// Reload animation speed multiplicator.
+        /// </summary>
         public float reloadAnimationSpeed = 1;
+
+        /// <summary>
+        /// Reload animation speed multiplicator.
+        /// </summary>
         public float fireAnimationSpeed = 1;
+
+        /// <summary>
+        /// Animation state for when the weapon is empty.
+        /// </summary>
+        [Header("Animation (Optional)")]
+        public string emptyState;
+
+        /// <summary>
+        /// Reload animation for when the weapon was shot empty.
+        /// </summary>
+        public string emptyReloadAnimation;
 
         protected override void OnWeaponReload()
         {
@@ -26,8 +55,13 @@ namespace OsFPS
             if (Essentials.UnityIsNull(this.animation))
                 return;
 
-            this.animation[this.reloadAnimation].speed = this.reloadAnimationSpeed;
-            this.animation.Play(this.reloadAnimation);
+            // Decide which animation to use
+            var animation = this.reloadAnimation;
+            if (this.weapon.ammoInClip <= 0 && !string.IsNullOrEmpty(this.emptyReloadAnimation))
+                animation = this.emptyReloadAnimation;
+
+            this.animation[animation].speed = this.reloadAnimationSpeed;
+            this.animation.Play(animation);
         }
 
         protected override void OnWeaponFireStart()
@@ -37,8 +71,13 @@ namespace OsFPS
             if (Essentials.UnityIsNull(this.animation))
                 return;
 
-            this.animation[this.fireAnimation].speed = this.fireAnimationSpeed;
-            this.animation.Play(this.fireAnimation);
+            // Decide which animation to use
+            var animation = this.fireAnimation;
+            if (this.weapon.ammoInClip <= 1 && !string.IsNullOrEmpty(this.emptyState))
+                animation = this.emptyState;
+
+            this.animation[animation].speed = this.fireAnimationSpeed;
+            this.animation.Play(animation);
         }
     }
 }
